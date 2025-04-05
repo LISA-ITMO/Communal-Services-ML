2f915d69acb381efae3317e4449eae8ff8dfd8ab
Goshective <den,skvortsoff@gmail.com>
2025-04-05 12:58:56 +0300

    Working version 1.0

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 from Data_Processing.create_converters import Converter
 from Models.load_model import Model
+from Data_Processing.texts import texts
 
 def read_texts(texts_name):
     PATH = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(PATH,texts_name), 'r') as f:
+    with open(os.path.join(PATH, 'Data_Processing', texts_name), 'r') as f:
         texts = f.readlines()
     return texts
 
@@ -18,6 +19,6 @@ if __name__ == "__main__":
 
     basic_converter = Converter(DB_NAME)
     basic_classifier = Model(REPO_NAME, basic_converter)
-    texts = read_texts(TEXTS_FILENAME)
+    # texts = read_texts(TEXTS_FILENAME)
 
     basic_classifier.print_prediction(texts, k=5)
