f7ac21ea6530ff63e472cc67a60a476ed80d6b0d
Goshective <den,skvortsoff@gmail.com>
2025-05-17 15:55:58 +0300

    Remove old file reading

--- main.py
+++ main.py
@@ -3,22 +3,14 @@ from Data_Processing.create_converters import Converter
 from Models.load_model import Model
 from Data_Processing.texts import texts
 
-def read_texts(texts_name):
-    PATH = os.path.dirname(os.path.abspath(__file__))
-    with open(os.path.join(PATH, 'Data_Processing', texts_name), 'r') as f:
-        texts = f.readlines()
-    return texts
-
 
 if __name__ == "__main__":
     DB_NAME = "db_2.csv"
     REPO_AUTHOR = "Goshective"
     REPO_DIR = "lab_comm_services_detailed_sber"
     REPO_NAME = f"{REPO_AUTHOR}/{REPO_DIR}"
-    TEXTS_FILENAME = "test_texts.txt"
 
     basic_converter = Converter(DB_NAME)
     basic_classifier = Model(REPO_NAME, basic_converter)
-    # texts = read_texts(TEXTS_FILENAME)
 
     basic_classifier.print_prediction(texts, k=5)
